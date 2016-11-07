f16e8a78fa156caae8513ee3933fed9e21a821b5
Figgynewts <znewton@iastate.edu>
2016-11-07 16:56:39 -0600

    Rendered head

--- lib/ViewRenderer.php
+++ lib/ViewRenderer.php
@@ -14,9 +14,29 @@ class ViewRenderer
 
     public function renderView($viewFile)
     {
+        echo $this->renderHead();
         echo $this->renderNavbar();
         set_include_path('');
         include 'views/'.$viewFile;
+    }
+
+    private function renderHead(){
+
+        return <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myFramework</title>
+	<meta name="description" content="My own framework with my own theme" />
+	<meta name="keywords" content="framework, theme, php, js, css, html" />
+	<meta name="author" content="znewton" />
+	<link rel="stylesheet" href="/lib/css/myFramework.css">
+	<script src="/lib/js/myFramework.js"></script>
+</head>
+HTML;
     }
 
     private function renderNavbar(){
