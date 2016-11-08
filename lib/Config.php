cf27a7b9900191b49c928127ec7e8d7aaa3bb68b
Figgynewts <znewton@iastate.edu>
2016-11-07 20:49:21 -0600

    Spaces to Tabs

--- lib/Config.php
+++ lib/Config.php
@@ -2,74 +2,74 @@
 
 class Config
 {
-    private $routeConfig = [
-        'test' => [
-            'uri' => '',
-            'action' => [
-                'view' => 'view.phtml',
-            ],
-        ],
-        'about' => [
-            'uri' => '',
-            'action' => [
-                'view' => 'about.phtml',
-            ],
-        ],
-        'testRest' => [
-            'uri' => '',
-            'action' => [
-                'rest_file' => 'RestTest.php',
-                'rest_class' => 'RestTest',
-            ],
-        ],
-        'testRoutes' => [
-            'uri' => 'test.php',
-            'sub_route' => [
-                'test' => [
-                    'uri' => 'test1.php',
-                    'sub_route' => [
-                        'test' => [
-                            'uri' => 'test2.php',
-                            'sub_route' => [
-                                'test' => [
-                                    'uri' => 'test3.php',
-                                    'sub_route' => [
-                                        'test' => [
-                                            'uri' => 'test4.php',
-                                        ],
-                                        'test1' => [
-                                            'uri' => 'test41.php',
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-            ],
-        ],
-    ];
-    function getRouteConfig(){
-        return $this->routeConfig;
-    }
+	private $routeConfig = [
+		'test' => [
+			'uri' => '',
+			'action' => [
+				'view' => 'view.phtml',
+			],
+		],
+		'about' => [
+			'uri' => '',
+			'action' => [
+				'view' => 'about.phtml',
+			],
+		],
+		'testRest' => [
+			'uri' => '',
+			'action' => [
+				'rest_file' => 'RestTest.php',
+				'rest_class' => 'RestTest',
+			],
+		],
+		'testRoutes' => [
+			'uri' => 'test.php',
+			'sub_route' => [
+				'test' => [
+					'uri' => 'test1.php',
+					'sub_route' => [
+						'test' => [
+							'uri' => 'test2.php',
+							'sub_route' => [
+								'test' => [
+									'uri' => 'test3.php',
+									'sub_route' => [
+										'test' => [
+											'uri' => 'test4.php',
+										],
+										'test1' => [
+											'uri' => 'test41.php',
+										],
+									],
+								],
+							],
+						],
+					],
+				],
+			],
+		],
+	];
+	function getRouteConfig(){
+		return $this->routeConfig;
+	}
 
-    private $viewConfig = [
-        'nav_links' => [
-            'about' => [
-                'label' => 'About',
-                'href' => '/about'
-            ],
-            'test' => [
-                'label' => 'Test',
-                'href' => '/test'
-            ]
-        ],
-        'drawer_links' => [],
-        'footer_contact' => [],
-        'footer_social' => [],
-    ];
+	private $viewConfig = [
+		'nav_links' => [
+			'about' => [
+				'label' => 'About',
+				'href' => '/about'
+			],
+			'test' => [
+				'label' => 'Test',
+				'href' => '/test'
+			]
+		],
+		'drawer_links' => [],
+		'footer_contact' => [],
+		'footer_social' => [],
+	];
 
-    function getViewConfig(){
-        return $this->viewConfig;
-    }
+	function getViewConfig(){
+		return $this->viewConfig;
+	}
 }
--- lib/ViewRenderer.php
+++ lib/ViewRenderer.php
@@ -4,33 +4,33 @@ include_once('Config.php');
 
 class ViewRenderer
 {
-    private $config = null;
+	private $config = null;
 
-    public function __construct()
-    {
-        $this->config = new Config();
-        $this->config = $this->config->getViewConfig();
-    }
+	public function __construct()
+	{
+		$this->config = new Config();
+		$this->config = $this->config->getViewConfig();
+	}
 
-    public function renderView($viewFile)
-    {
-        echo $this->renderHead();
-        echo $this->renderNavbar();
-        echo $this->renderDrawer();
-        set_include_path('');
-        include 'views/'.$viewFile;
-    }
+	public function renderView($viewFile)
+	{
+		echo $this->renderHead();
+		echo $this->renderNavbar();
+		echo $this->renderDrawer();
+		set_include_path('');
+		include 'views/'.$viewFile;
+	}
 
-    private function renderHead(){
+	private function renderHead(){
 
-        return <<<HTML
+		return <<<HTML
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>myFramework</title>
+	<title>myFramework</title>
 	<meta name="description" content="My own framework with my own theme" />
 	<meta name="keywords" content="framework, theme, php, js, css, html" />
 	<meta name="author" content="znewton" />
@@ -38,46 +38,46 @@ class ViewRenderer
 	<script src="/lib/js/myFramework.js"></script>
 </head>
 HTML;
-    }
+	}
 
-    private function renderNavbar(){
-        $links = [];
-        foreach ($this->config['nav_links'] as $link){
-            $links[] = <<<HTML
+	private function renderNavbar(){
+		$links = [];
+		foreach ($this->config['nav_links'] as $link){
+			$links[] = <<<HTML
 <a href="{$link['href']}">{$link['label']}</a>
 HTML;
-        }
-        $links = implode("\n",$links);
-        return <<<HTML
+		}
+		$links = implode("\n",$links);
+		return <<<HTML
 <nav id="navbar">
-    <div class="left-nav">
-            <div class="ham-menu"><span></span><span></span><span></span></div>
-            <a href="/" class="home-link">Home</a>
-    </div>
-    <div class="right-nav">
-         {$links}
-    </div>
+	<div class="left-nav">
+			<div class="ham-menu"><span></span><span></span><span></span></div>
+			<a href="/" class="home-link">Home</a>
+	</div>
+	<div class="right-nav">
+		 {$links}
+	</div>
 </nav>
 HTML;
 
-    }
-    private function renderDrawer(){
-        $links = [];
-        foreach ($this->config['nav_links'] as $link){
-            $links[] = <<<HTML
+	}
+	private function renderDrawer(){
+		$links = [];
+		foreach ($this->config['nav_links'] as $link){
+			$links[] = <<<HTML
 <a href="{$link['href']}">{$link['label']}</a>
 HTML;
-        }
-        $links = implode("\n",$links);
-        return <<<HTML
+		}
+		$links = implode("\n",$links);
+		return <<<HTML
 <nav id="drawer">
-    <a href="/">Home</a>
-    {$links}
+	<a href="/">Home</a>
+	{$links}
 </nav>
 HTML;
 
-    }
-    private function renderFooter(){
+	}
+	private function renderFooter(){
 
-    }
+	}
 }
